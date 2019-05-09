8b2474b46908629dc5a05ff6bb21cf8491af76e4
lena <aartisdead@gmail.com>
2019-05-09 18:36:32 +0300

    routing, img-icon

--- IPZ_MovieProj/ClientApp/src/app/app-routing.module.ts
+++ IPZ_MovieProj/ClientApp/src/app/app-routing.module.ts
@@ -3,9 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { FilmComponent } from './film/film.component';
 import { RatingVotingComponent} from './rating-voting/rating-voting.component'
 import { FilmsComponent } from './films/films.component';
+import { Film } from './film';
 
 const routes: Routes = [
-  {path: 'films', component: FilmsComponent}
+  {path:'', redirectTo: '/films', pathMatch: 'full'},
+  {path: 'films', component: FilmsComponent},
+  {path: ':id', component: FilmComponent}
 ]
 
 @NgModule({
