d148e6c43de6a955a91f13af97173ac4a864ba24
Crimson Dwarf <crimsondwarfy@gmial.com>
2019-05-15 01:15:07 +0300

    Backend authorisation .gitignore modified

--- Entities/User.cs
+++ Entities/User.cs
@@ -4,13 +4,13 @@ using System.Text;
 
 namespace Entities
 {
-    public class User
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string UserImgPath { get; set; }
-        public IEnumerable<Comment> Comments { get; set; }
-        public IEnumerable<Vote> Votes { get; set; }
+	public class User
+	{
+		public int Id { get; set; }
+		public string UserName { get; set; }
+		public byte[] UserPic { get; set; }
+		public IEnumerable<Comment> Comments { get; set; }
+		public IEnumerable<Vote> Votes { get; set; }
 
-    }
+	}
 }
---
+++ IPZ_MovieProj/Controllers/AuthorisationController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Interfaces;
+using Entities;
+using IPZ_MovieProj.Services.Authorisation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IPZ_MovieProj.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthorisationController : ControllerBase
+	{
+		private readonly SignInManager<AuthUser> _signInManager;
+		private readonly UserManager<AuthUser> _userManager;
+		private readonly IConfiguration _configuration;
+		private readonly IUserService _appUserService;
+
+		public AuthorisationController(SignInManager<AuthUser> signInManager,
+			UserManager<AuthUser> userManager,
+			IUserService appUserService,
+			IConfiguration configuration)
+		{
+			_signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+			_userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+			_configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+			_appUserService = appUserService ?? throw new ArgumentNullException(nameof(appUserService));
+		}
+
+		[HttpPost("sign-in")]
+		[AllowAnonymous]
+		public async Task<IActionResult> SignInAsync([FromBody] SignInModel signInModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+
+			Microsoft.AspNetCore.Identity.SignInResult result =
+				await _signInManager.PasswordSignInAsync(signInModel.Login, signInModel.Password, false, false);
+
+			if (!result.Succeeded)
+			{
+				return Unauthorized();
+			}
+
+			AuthUser authUser = _userManager.Users.FirstOrDefault(user => user.UserName == signInModel.Login);
+			User appUser = await _appUserService.GetByIdAsync(authUser.Id);
+			string token = GenerateJwtTokenString(authUser);
+
+			return Ok(new { user = appUser, token });
+		}
+
+		[HttpPost("sign-up")]
+		public async Task<IActionResult> SignUp([FromBody] SignUpModel signUpModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			var authUser = new AuthUser
+			{
+				UserName = signUpModel.SignInModel.Login
+			};
+
+			var result = await _userManager.CreateAsync(authUser, signUpModel.SignInModel.Password);
+
+			if (!result.Succeeded)
+			{
+				return BadRequest(result.Errors);
+			}
+
+			await _signInManager.SignInAsync(authUser, false);
+			int authUserId = _userManager.Users.FirstOrDefault(user => user.UserName == authUser.UserName).Id;
+
+			var appUser = new User
+			{
+				Id = authUserId,
+				UserName = signUpModel.SignInModel.Login
+				//TODO: add properties of user
+			};
+			using (var memoryStream = new MemoryStream())
+			{
+				await signUpModel.UserPic.CopyToAsync(memoryStream);
+				appUser.UserPic = memoryStream.ToArray();
+			}
+
+			await _appUserService.AddUserAsync(appUser);
+
+			string token = GenerateJwtTokenString(authUser);
+
+			return Ok(new { user = appUser, token });
+		}
+
+		private string GenerateJwtTokenString(AuthUser user)
+		{
+			long unixNowSeconds = DateTimeOffset.Now.ToUnixTimeSeconds();
+			long expirationTime = unixNowSeconds + 10 * 3600;
+
+			var claims = new List<Claim>
+			{
+                // https://openid.net/specs/openid-connect-core-1_0.html#IDToken
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                //new Claim(JwtRegisteredClaimNames.Iss, _configuration["Jwt:Issuer"]),
+                //new Claim(JwtRegisteredClaimNames.Aud, _configuration["Jwt:Issuer"]),
+                new Claim(JwtRegisteredClaimNames.Iat, unixNowSeconds.ToString()),
+                //new Claim(JwtRegisteredClaimNames.Exp, expirationTime.ToString()),
+                // new Claim(ClaimTypes.Role, user.)
+            };
+
+			var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+			var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+			var token = new JwtSecurityToken(
+				_configuration["Jwt:Issuer"],
+				_configuration["Jwt:Audience"],
+				claims,
+				expires: DateTime.Now.AddMinutes(10),
+				signingCredentials: credentials
+			);
+
+			return new JwtSecurityTokenHandler().WriteToken(token);
+		}
+	}
+}
