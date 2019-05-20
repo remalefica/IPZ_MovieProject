719f85ed128406ad37de739a6e37473d0dcf9e0f
lena <aartisdead@gmail.com>
2019-05-21 01:13:03 +0300

    database update

--- DAL/EF/AppDbContext.cs
+++ DAL/EF/AppDbContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using static System.Net.WebRequestMethods;
 
 namespace DAL
 {
@@ -51,7 +52,7 @@ namespace DAL
 					Director = "Robert Zemeckis",
 					Year = 1994,
 					Description = "The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other history unfold through the perspective of an Alabama man with an IQ of 75.",
-					FilmImgPath = "https://www.imdb.com/title/tt0109830/mediaviewer/rm1954748672",
+					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
 					FilmTrailerPath = "https://www.youtube.com/embed/eYSnxZKTZzU",
 					OriginCountry = "USA",
 					Budget = 55000000,
@@ -64,9 +65,10 @@ namespace DAL
 					Director = "David Fincher",
 					Year = 1999,
 					Description = "An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.",
-					FilmImgPath = "https://www.imdb.com/title/tt0137523/mediaviewer/rm590641920",
-					FilmTrailerPath = "https://www.imdb.com/title/tt0137523/videoplayer/vi781228825?ref_=tt_ov_vi",
-					OriginCountry = "USA | Germany",
+					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BMjJmYTNkNmItYjYyZC00MGUxLWJhNWMtZDY4Nzc1MDAwMzU5XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,676,1000_AL_.jpg",
+					FilmTrailerPath = "<iframe src='https://www.imdb.com/videoembed/vi781228825' allowfullscreen width='854' height='400'></iframe>",
+
+                    OriginCountry = "USA | Germany",
 					Budget = 63000000,
 					DurationInMinutes = 112
 				},
@@ -77,9 +79,10 @@ namespace DAL
 					Director = "Christopher Nolan",
 					Year = 1,
 					Description = "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
-					FilmImgPath = "https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392",
-                    FilmTrailerPath = "https://www.imdb.com/title/tt1375666/videoplayer/vi4219471385?ref_=tt_ov_vi",
-					OriginCountry = "USA | UK",
+					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+                    FilmTrailerPath = "<iframe src='Http://www.imdb.com/videoembed/vi4219471385' allowfullscreen width='854' height='400'></iframe>",
+
+                    OriginCountry = "USA | UK",
 					Budget = 160000000,
 					DurationInMinutes = 148
 				},
@@ -90,9 +93,10 @@ namespace DAL
 					Director = "Christopher Nolan",
 					Year = 2008,
 					Description = "It's tough to picture anyone other than Christian Bale as Batman in Christopher Nolan's trilogy, but plenty of other actors were considered. See who they were.",
-					FilmImgPath = "https://www.imdb.com/title/tt0468569/mediaviewer/rm4023877632",
-					FilmTrailerPath = "https://www.imdb.com/title/tt0468569/videoplayer/vi324468761?ref_=tt_ov_vi",
-					OriginCountry = "USA | UK",
+					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+					FilmTrailerPath = "<iframe src='https://www.imdb.com/videoembed/vi324468761' allowfullscreen width='854' height='400'></iframe>",
+
+                    OriginCountry = "USA | UK",
 					Budget = 185000000,
 					DurationInMinutes = 152
 				},
@@ -117,8 +121,8 @@ namespace DAL
 					Year = 2011,
 					Description = "After he becomes a quadriplegic from a paragliding accident, an aristocrat hires a young man from the projects to be his caregiver.",
 					FilmImgPath = "https://filmix.co/uploads/posters/big/neprikasaemye-2011_34099_0.jpg",
-					FilmTrailerPath = "https://www.youtube.com/embed/qCSW7hqBMTg",
-					OriginCountry = "France",
+					FilmTrailerPath = "<iframe src='https://www.imdb.com/videoembed/vi59285529' allowfullscreen width='854' height='400'></iframe>",
+                    OriginCountry = "France",
 					Budget = 9500000,
 					DurationInMinutes = 112
 				},
@@ -130,8 +134,9 @@ namespace DAL
 					Year = 1998,
 					Description = "Jeff The Dude Lebowski, mistaken for a millionaire of the same name, seeks restitution for his ruined rug and enlists his bowling buddies to help get it.",
 					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BMTQ0NjUzMDMyOF5BMl5BanBnXkFtZTgwODA1OTU0MDE@._V1_SY1000_CR0,0,670,1000_AL_.jpg",
-					FilmTrailerPath = "https://www.imdb.com/videoembed/vi4018733337",
-					OriginCountry = "USA | UK",
+					FilmTrailerPath = "<iframe src='https://www.imdb.com/videoembed/vi4018733337' allowfullscreen width='854' height='400'></iframe>",
+
+                    OriginCountry = "USA | UK",
                     Budget = 15000000,
 					DurationInMinutes = 117
 				},
@@ -142,9 +147,10 @@ namespace DAL
 					Director = "Martin Scorsese",
 					Year = 2013,
 					Description = "Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.",
-					FilmImgPath = "https://www.imdb.com/title/tt0993846/mediaviewer/rm2842940160",
-					FilmTrailerPath = "https://www.imdb.com/title/tt0993846/videoplayer/vi2312218649?ref_=tt_ov_vi",
-					OriginCountry = "USA",
+					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BMjIxMjgxNTk0MF5BMl5BanBnXkFtZTgwNjIyOTg2MDE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+					FilmTrailerPath = "<iframe src='https://www.imdb.com/videoembed/vi2312218649' allowfullscreen width='854' height='400'></iframe>",
+
+                    OriginCountry = "USA",
 					Budget = 100000000,
 					DurationInMinutes = 180
 				},
@@ -155,9 +161,10 @@ namespace DAL
 					Director = "Peter Jackson",
 					Year = 2003,
 					Description = "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
-					FilmImgPath = "https://www.imdb.com/title/tt0167260/mediaviewer/rm584928512",
-					FilmTrailerPath = "https://www.imdb.com/title/tt0167260/videoplayer/vi2073101337?ref_=tt_ov_vi",
-					OriginCountry = "New Zealand | USA",
+					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+					FilmTrailerPath = "<iframe src='https://www.imdb.com/videoembed/vi2073101337' allowfullscreen width='854' height='400'></iframe>",
+
+                    OriginCountry = "New Zealand | USA",
 					Budget = 94000000,
 					DurationInMinutes = 201
 				},
@@ -168,9 +175,10 @@ namespace DAL
 					Director = "Lana Wachowski (as The Wachowski Brothers), Lilly Wachowski (as The Wachowski Brothers)",
 					Year = 1999,
 					Description = "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
-					FilmImgPath = "https://www.imdb.com/title/tt0133093/mediaviewer/rm525547776",
-					FilmTrailerPath = "https://www.imdb.com/title/tt0133093/videoplayer/vi1032782617?ref_=tt_ov_vi",
-					OriginCountry = "USA",
+					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
+					FilmTrailerPath = "<iframe src='https://www.imdb.com/videoembed/vi1032782617' allowfullscreen width='854' height='400'></iframe>",
+
+                    OriginCountry = "USA",
 					Budget = 63000000,
 					DurationInMinutes = 136
 				},
@@ -181,9 +189,10 @@ namespace DAL
 					Director = "Jon Watts",
 					Year = 2017,
 					Description = "Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.",
-					FilmImgPath = "https://www.imdb.com/title/tt2250912/mediaviewer/rm3491180544",
-					FilmTrailerPath = "https://www.imdb.com/title/tt2250912/videoplayer/vi4175083801?ref_=tt_ov_vi",
-					OriginCountry = "USA",
+					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BNTk4ODQ1MzgzNl5BMl5BanBnXkFtZTgwMTMyMzM4MTI@._V1_SY1000_CR0,0,658,1000_AL_.jpg",
+					FilmTrailerPath = "<iframe src='https://www.imdb.com/videoembed/vi4175083801' allowfullscreen width='854' height='400'></iframe>",
+
+                    OriginCountry = "USA",
 					Budget = 175000000,
 					DurationInMinutes = 133
 				},
@@ -194,9 +203,10 @@ namespace DAL
 					Director = "Tomm Moore",
 					Year = 2014,
 					Description = "Ben, a young Irish boy, and his little sister Saoirse, a girl who can turn into a seal, go on an adventure to free the fairies and save the spirit world.",
-					FilmImgPath = "https://www.imdb.com/title/tt1865505/mediaviewer/rm1350432768",
-					FilmTrailerPath = "https://www.imdb.com/title/tt1865505/videoplayer/vi3133192729?ref_=tt_ov_vi",
-					OriginCountry = "Ireland | Denmark | Belgium | Luxembourg | France",
+					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BMTQ2MDMwNjEwNV5BMl5BanBnXkFtZTgwOTkxMzI0MzE@._V1_SY1000_CR0,0,691,1000_AL_.jpg",
+					FilmTrailerPath = "<iframe src='https://www.imdb.com/videoembed/vi3133192729' allowfullscreen width='854' height='400'></iframe>",
+
+                    OriginCountry = "Ireland | Denmark | Belgium | Luxembourg | France",
 					Budget = 857524,
 					DurationInMinutes = 93
 				},
@@ -206,69 +216,65 @@ namespace DAL
 					Name = "Bohemian Rhapsody",
 					Director = "Bryan Singer",
 					Year = 2018,
-					Description = "",
-					FilmImgPath = "https://images.gowatchit.com/posters/original/p15987_p_v7_aa.jpg?1473180482",
-					FilmTrailerPath = "",
-					OriginCountry = "",
-					Budget = 1,
-					RatingAvg = 1,
-					DurationInMinutes = 1
+					Description = "The story of the legendary rock band Queen and lead singer Freddie Mercury, leading up to their famous performance at Live Aid (1985).",
+					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BMTA2NDc3Njg5NDVeQTJeQWpwZ15BbWU4MDc1NDcxNTUz._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+					FilmTrailerPath = "<iframe src='https://www.imdb.com/videoembed/vi1451538969' allowfullscreen width='854' height='400'></iframe>",
+
+                    OriginCountry = "UK | USA",
+					Budget = 52000000,
+					DurationInMinutes = 134
 				},
 				new Film
 				{
 					Id = 15,
-					Name = "j",
-					Director = "",
-					Year = 1,
-					Description = "",
-					FilmImgPath = "https://images.gowatchit.com/posters/original/p15987_p_v7_aa.jpg?1473180482",
-					FilmTrailerPath = "",
-					OriginCountry = "",
-					Budget = 1,
-					RatingAvg = 1,
-					DurationInMinutes = 1
+					Name = "Dunkirk",
+					Director = "Christopher Nolan",
+					Year = 2017,
+					Description = "Allied soldiers from Belgium, the British Empire, and France are surrounded by the German Army, and evacuated during a fierce battle in World War II.",
+					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BN2YyZjQ0NTEtNzU5MS00NGZkLTg0MTEtYzJmMWY3MWRhZjM2XkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+					FilmTrailerPath = "<iframe src='https://www.imdb.com/videoembed/vi3402283289' allowfullscreen width='854' height='400'></iframe>",
+
+                    OriginCountry = "UK | Netherlands | France | USA",
+					Budget = 100000000,
+					DurationInMinutes = 106
 				},
 				new Film
 				{
 					Id = 16,
-					Name = "k",
-					Director = "",
-					Year = 1,
-					Description = "",
-					FilmImgPath = "https://images.gowatchit.com/posters/original/p15987_p_v7_aa.jpg?1473180482",
-					FilmTrailerPath = "",
-					OriginCountry = "",
-					Budget = 1,
-					RatingAvg = 1,
-					DurationInMinutes = 1
+					Name = "The Lion King",
+					Director = "Jon Favreau",
+					Year = 2019,
+					Description = "After the murder of his father, a young lion prince flees his kingdom only to learn the true meaning of responsibility and bravery.",
+					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+					FilmTrailerPath = "<iframe src='https://www.imdb.com/videoembed/vi1166654489' allowfullscreen width='854' height='400'></iframe>",
+                    OriginCountry = " USA"
 				},
 				new Film
 				{
 					Id = 17,
-					Name = "l",
-					Director = "",
-					Year = 1,
-					Description = "",
-					FilmImgPath = "https://images.gowatchit.com/posters/original/p15987_p_v7_aa.jpg?1473180482",
-					FilmTrailerPath = "",
-					OriginCountry = "",
-					Budget = 1,
-					RatingAvg = 1,
-					DurationInMinutes = 1
+					Name = "Scream",
+					Director = "Wes Craven",
+					Year = 1996,
+					Description = "A year after the murder of her mother, a teenage girl is terrorized by a new killer, who targets the girl and her friends by using horror films as part of a deadly game.",
+					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BMjA2NjU5MTg5OF5BMl5BanBnXkFtZTgwOTkyMzQxMDE@._V1_SY1000_CR0,0,673,1000_AL_.jpg",
+					FilmTrailerPath = "<iframe src='https://www.imdb.com/videoembed/vi1175821337' allowfullscreen width='854' height='400'></iframe>",
+                    OriginCountry = "USA",
+                    Budget = 14000000,
+					DurationInMinutes = 111
 				},
 				new Film
 				{
 					Id = 18,
-					Name = "m",
-					Director = "",
-					Year = 1,
-					Description = "",
-					FilmImgPath = "https://images.gowatchit.com/posters/original/p15987_p_v7_aa.jpg?1473180482",
-					FilmTrailerPath = "",
-					OriginCountry = "",
-					Budget = 1,
-					RatingAvg = 1,
-					DurationInMinutes = 1
+					Name = "La La Land",
+					Director = "Damien Chazelle",
+					Year = 2016,
+					Description = "While navigating their careers in Los Angeles, a pianist and an actress fall in love while attempting to reconcile their aspirations for the future.",
+					FilmImgPath = "https://m.media-amazon.com/images/M/MV5BMzUzNDM2NzM2MV5BMl5BanBnXkFtZTgwNTM3NTg4OTE@._V1_SY1000_SX675_AL_.jpg",
+					FilmTrailerPath = "<iframe src='https://www.imdb.com/videoembed/vi2514728473' allowfullscreen width='854' height='400'></iframe>",
+
+                    OriginCountry = "USA | Hong Kong",
+					RatingAvg = 30000000,
+					DurationInMinutes = 128
 				},
 				new Film
 				{
@@ -277,11 +283,10 @@ namespace DAL
 					Director = "",
 					Year = 1,
 					Description = "",
-					FilmImgPath = "https://images.gowatchit.com/posters/original/p15987_p_v7_aa.jpg?1473180482",
+					FilmImgPath = "",
 					FilmTrailerPath = "",
 					OriginCountry = "",
 					Budget = 1,
-					RatingAvg = 1,
 					DurationInMinutes = 1
 				},
 				new Film
@@ -291,11 +296,10 @@ namespace DAL
 					Director = "",
 					Year = 1,
 					Description = "",
-					FilmImgPath = "https://images.gowatchit.com/posters/original/p15987_p_v7_aa.jpg?1473180482",
+					FilmImgPath = "",
 					FilmTrailerPath = "",
 					OriginCountry = "",
 					Budget = 1,
-					RatingAvg = 1,
 					DurationInMinutes = 1
 				});
 
