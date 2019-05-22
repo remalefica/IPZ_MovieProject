5a1d9518181f149129f58ba826c331260cc74cd5
Crimson Dwarf <crimsondwarfy@gmial.com>
2019-05-22 16:01:04 +0300

    Nav-bar modified

--- IPZ_MovieProj/ClientApp/src/app/components/comment/comment.component.ts
+++ IPZ_MovieProj/ClientApp/src/app/components/comment/comment.component.ts
@@ -4,6 +4,8 @@ import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { Film } from '../../models/film';
 import { CommentService } from '../../services/comment/comment.service';
 import { filterQueryId } from '@angular/core/src/view/util';
+import { AuthService } from 'src/app/services/authorisation/authorisation.service';
+import { MessageService } from 'src/app/services/message/message.service';
 
 @Component({
   selector: 'app-comment',
@@ -12,23 +14,32 @@ import { filterQueryId } from '@angular/core/src/view/util';
 })
 export class CommentComponent implements OnInit {
 
-  constructor(private commentService: CommentService) { }
+  constructor(private commentService: CommentService,
+              private authService: AuthService,
+              private messageService: MessageService) { }
 
   comment: CommentFilm;
   @Input() filmId : number;
 
   ngOnInit() {
-    this.newFilm();
+    this.newComment();
   }
 
-  // sendComment() : void{
-  //   this.commentService.addComment(this.comment);
-  //   this.newFilm();
-  // }
+   sendComment() : void{
+     this.commentService.addComment(this.comment);
+     this.newComment();
+   }
 
-  newFilm() : void{
+  newComment() : void{
     this.comment = new CommentFilm();
     this.comment.filmId = this.filmId;
+    if (!this.authService.isSignedIn())
+    {
+      this.messageService.add("You`re not signed in");
+      return;
+    }
+    this.authService.getCurrentUser()
+          .subscribe(user => this.comment.userId = user.id);
   }
 
 }
