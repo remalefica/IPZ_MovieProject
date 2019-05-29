5b30012626c28901b72d17ac9279483ce688bde0
Vladislava Sheliakhina <vladislava0810@gmail.com>
2019-05-29 23:04:07 +0300

    last ver

--- IPZ_MovieProj/ClientApp/src/app/services/rating/rating-voting.service.ts
+++ IPZ_MovieProj/ClientApp/src/app/services/rating/rating-voting.service.ts
@@ -27,7 +27,7 @@ export class RatingVotingService {
   addVote(rating : VoteFilm) : Observable<VoteFilm>{
     let PATH = this.url +'/Create';
     this.messageService.add('Your vote was added.');
-    return this.httpClient.post<VoteFilm>(PATH, rating)
+    return this.httpClient.post<VoteFilm>(PATH, rating);
   }
 
   updateVote(id : number, rating : VoteFilm) : Observable<VoteFilm>{
